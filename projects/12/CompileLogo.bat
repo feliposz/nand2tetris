eb508699e516a3977294735caac8c1ac56688c7e
feliposz <feliposz@gmail.com>
2020-04-10 23:09:33 -0300

    project 12: added standard error checks

--- projects/12/CompileLogo.bat
+++ projects/12/CompileLogo.bat
@@ -3,4 +3,6 @@ md Logo
 del /q Logo\*.*
 copy ..\09\Logo\*.jack Logo
 copy *.jack Logo
+rem call ..\..\tools\JackCompiler.bat Logo
 call node ..\11\JackCompiler Logo
+rem call node ..\08\vmtranslator Logo
--- projects/12/CompileTests.bat
+++ projects/12/CompileTests.bat
@@ -15,26 +15,27 @@ call ..\..\tools\JackCompiler.bat SysTest
 echo.
 echo Compiling implementations
 echo =========================
+del MathTest\Math.vm
 call ..\..\tools\JackCompiler.bat Math.jack
 if %errorlevel% equ 0 copy Math.vm MathTest
-
+del MemoryTest\Memory.vm
 call ..\..\tools\JackCompiler.bat Memory.jack
 if %errorlevel% equ 0 copy Memory.vm MemoryTest
-
+del ScreenTest\Screen.vm
 call ..\..\tools\JackCompiler.bat Screen.jack
 if %errorlevel% equ 0 copy Screen.vm ScreenTest
-
+del StringTest\String.vm
 call ..\..\tools\JackCompiler.bat String.jack
 if %errorlevel% equ 0 copy String.vm StringTest
-
+del OutputTest\Output.vm
 call ..\..\tools\JackCompiler.bat Output.jack
 if %errorlevel% equ 0 copy Output.vm OutputTest
-
+del KeyboardTest\Keyboard.vm
 call ..\..\tools\JackCompiler.bat Keyboard.jack
 if %errorlevel% equ 0 copy Keyboard.vm KeyboardTest
-
+del ArrayTest\Array.vm
 call ..\..\tools\JackCompiler.bat Array.jack
 if %errorlevel% equ 0 copy Array.vm ArrayTest
-
+del SysTest\Sys.vm
 call ..\..\tools\JackCompiler.bat Sys.jack
 if %errorlevel% equ 0 copy Sys.vm SysTest
